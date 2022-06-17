d3e3736a0596832cd2665715ac464725a3dc4244
chowdary0512 <chowdary0512@gmail>
2022-06-17 22:36:09 +0530

    practice

---
+++ continue.py
@@ -0,0 +1,17 @@
+for i in "Draksha":
+    if i=='k':
+        break
+    print(i)
+print("the end")
+
+for i in "Draksha":
+    if i=='k':
+        continue
+    print(i)
+print("the end")
+
+for i in "Chowdary":
+    if i=='o':
+        print(i)
+    else:
+        pass
---
+++ string.py
@@ -0,0 +1,21 @@
+#guyjgujyg
+''' yhukhskhdgikhedgio
+jgafukhsjkhfsh
+sdfgjishfjksdhg'''
+s='Draksha'
+print(s)
+print(s.upper())
+print(s.lower())
+s1="    draksha chowdary here  "
+s1=s1.split()
+print(type(s1),s1)
+for i in s1:
+    print(i)
+s2="   python programming is easy    "
+print(s2.strip())
+print(s2.rstrip())
+print(s2.lstrip())
+print(s.startswith('k'))
+print(s.endswith('a'))
+print(s.count('a'))
+print(s2.replace("programming","naidu"))
